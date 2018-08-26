912b70ed4849990bbd8eddef4304174d8a06005c
mbdavid <mauricio.david@gmail.com>
2018-08-26 10:02:19 -0300

    Add encoding test

---
+++ WebApp/Components/Encoding.vue
@@ -0,0 +1,10 @@
+﻿@viewmodel WebApp.Components.EncodingVM
+
+<template>
+    <div class="item">
+        <h1>Hèllo Wôrld - Mãçâ</h1>
+        <hr/>
+        <h3>{{ Text }}</h3>
+        <button @click="FromServer">ClickMe-Server</button>
+    </div>
+</template>
---
+++ WebApp/Components/Encoding.vue.cs
@@ -0,0 +1,17 @@
+﻿using DotVue;
+using System;
+
+namespace WebApp.Components
+{
+    public class EncodingVM : ViewModel
+    {
+        public string Text { get; set; }
+
+        public void FromServer()
+        {
+            this.Text = "Hèllo Wôrld - Mãçâ";
+
+            this.ClientScript.Alert(this.Text);
+        }
+    }
+}
